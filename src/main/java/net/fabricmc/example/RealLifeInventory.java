d52281ed13b6f699bcefce0469931c4c7daae5b4
Yash Shah <ys562@cam.ac.uk>
2021-03-26 14:46:38 +0000

    Added template mod

---
+++ src/main/java/net/fabricmc/example/RealLifeInventory.java
@@ -0,0 +1,22 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class RealLifeInventory implements ModInitializer {
+	// an instance of our new item
+	public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), FABRIC_ITEM);
+		System.out.println("Hello Fabric world!");
+	}
+}
